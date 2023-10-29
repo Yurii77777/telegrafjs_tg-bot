6343d04af8111ee5b1621e47440dfb854408c804
kyiv <yuretshome@gmail.com>
2023-10-29 20:44:08 +0200

    Add a 'start' command for the bot

--- service/telegram.service/mainTelegram.service.ts
+++ service/telegram.service/mainTelegram.service.ts
@@ -3,31 +3,29 @@ const { Scenes, session } = require('telegraf');
 import { bot } from '../../config/telegram.config';
 
 import { TelegramCommandsService } from './telegramCommands.service';
-import { TelegramUtilsService } from './telegramUtils.service';
-import { TelegramActionsService } from './telegramActions.service';
-import { TelegramUpdateUserScene } from './scenes/updateUserScene.service';
-import { TelegramAddPostScene } from './scenes/addPostScene.service';
-
-import { UserService } from '../user.service';
-import { TELEGRAM_BOT_SCENES } from '../../constants/telegramBotScenes';
-import { TELEGRAM_BOT_BTN_ACTIONS } from '../../constants/telegramBotBtnActions';
 
 export class TelegramService {
   private telegramCommandsService: TelegramCommandsService;
-  private telegramUtilsService: TelegramUtilsService;
-  private telegramActionsService: TelegramActionsService;
-  private telegramUpdateUserScene: TelegramUpdateUserScene;
-  private telegramAddPostScene: TelegramAddPostScene;
-  private userService: UserService;
 
   constructor() {
     this.telegramCommandsService = new TelegramCommandsService();
-    this.telegramUtilsService = new TelegramUtilsService();
-    this.telegramActionsService = new TelegramActionsService();
-    this.telegramUpdateUserScene = new TelegramUpdateUserScene();
-    this.telegramAddPostScene = new TelegramAddPostScene();
-    this.userService = new UserService();
   }
 
-  async handleBotEvents() {}
+  async handleBotEvents() {
+    try {
+      await this.telegramCommandsService.setBotCommands();
+    } catch (error) {
+      console.log('[error]', error);
+    }
+
+    bot.command('start', async (ctx: any) => {
+      try {
+        await this.telegramCommandsService.handleStartCommand(ctx);
+      } catch (error) {
+        console.log('Error handleStartCommand :::', error);
+      }
+    });
+
+    bot.launch();
+  }
 }
--- service/telegram.service/telegramCommands.service.ts
+++ service/telegram.service/telegramCommands.service.ts
@@ -6,16 +6,33 @@ import { bot } from '../../config/telegram.config';
 import { User } from '../../model/user.model';
 
 import { UserService } from '../user.service';
-import { TelegramUtilsService } from '../telegram.service/telegramUtils.service';
+// import { TelegramUtilsService } from '../telegram.service/telegramUtils.service';
 
 import { TELEGRAM_BOT_BTN_ACTIONS } from '../../constants/telegramBotBtnActions';
 
 export class TelegramCommandsService {
   private userService: UserService;
-  private telegramUtilsService: TelegramUtilsService;
+  // private telegramUtilsService: TelegramUtilsService;
 
   constructor() {
     this.userService = new UserService();
-    this.telegramUtilsService = new TelegramUtilsService();
+    // this.telegramUtilsService = new TelegramUtilsService();
+  }
+
+  async setBotCommands() {
+    bot.telegram.setMyCommands([
+      {
+        command: '/start',
+        description: "Для старту | Let's start",
+      },
+    ]);
+  }
+
+  async handleStartCommand(ctx) {
+    try {
+      await ctx.reply('Привіт!');
+    } catch (error) {
+      console.log('Error send message :::', error);
+    }
   }
 }
