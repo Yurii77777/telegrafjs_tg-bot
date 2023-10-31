810bf30c849d60a89cd2619584178a4a751fd310
kyiv <yuretshome@gmail.com>
2023-10-31 19:09:25 +0200

    Update 'start' command

--- service/telegram.service/telegramCommands.service.ts
+++ service/telegram.service/telegramCommands.service.ts
@@ -33,10 +33,87 @@ export class TelegramCommandsService {
   }
 
   async handleStartCommand(ctx) {
+    // Розкоментуйте рядок нижче, щоб побачити дані
+    // Виведіть весь ctx, а не лише вкладені секції
+    // console.log('[ctx.update.message in start command]', ctx?.update?.message);
+
+    // Дістаємо необхідні нам дані із ctx
+    // Для зручності id перейменуємо в chatId
+    const { id: chatId, first_name, username } = ctx?.update?.message?.chat;
+
+    // Робимо запит до бази даних для пошуку такого користувача
+    let user: null | User = null;
+
     try {
-      await ctx.reply('Привіт!');
+      user = await this.userService.getUser({ chatId });
     } catch (error) {
-      console.log('Error send message :::', error);
+      // Якщо ми потрапили в дану секцію, значить запит до БД впав
+      // Про це необхідно сповістити користувача, оскільки він то не знає цього
+      // Для цого ми смотрюємо окремий метод, щоб було зручно відправляти повідомлення
+      // в залежності від обраної мови користувачем
+      // Але в даному випадку, ми не знайшли юзера, тому за замовчуванням ставимо ENG
+      ctx.reply(this.telegramUtilsService.setBotMessage({ lang: 'ENG', message: 'DB_REQUEST_ERROR' }));
+    }
+
+    // Якщо користувача не має в базі даних
+    if (!user) {
+      // Створюємо нового користувачв в базі даних
+      try {
+        await this.userService.createUser({ chatId, telegramName: first_name || username });
+      } catch (error) {
+        // Якщо щось піде не так, то ми опинимося тут
+        ctx.reply(this.telegramUtilsService.setBotMessage({ lang: 'ENG', message: 'DB_REQUEST_ERROR' }));
+
+        // Далі нічого не робимо, оскільки користувач повинен почати все спочатку
+        return;
+      }
+
+      // Формуємо наше повідомлення для відповіді юзеру із окремих сегментів, для наглядності
+      let messageToUser = this.telegramUtilsService.setBotMessage({
+        lang: 'UA',
+        message: 'SUCCESS_USER_CREATE',
+      });
+      messageToUser += `${this.telegramUtilsService.setBotMessage({
+        lang: 'UA',
+        message: 'YOUR_CHATID',
+      })}${chatId}\n`;
+      messageToUser += `${this.telegramUtilsService.setBotMessage({
+        lang: 'UA',
+        message: 'YOUR_NAME',
+      })}${first_name || username}\n`;
+
+      // Бот надсилає повідомлення про успіх,
+      // оскільки якщо ми дійшли сюди, то користувач був створений в базі даних!
+      try {
+        ctx.reply(messageToUser);
+      } catch (error) {
+        console.log('[Error send message] ::: ', error);
+      }
+    }
+    // Обробники для користувача, який вже є в базі даних
+    else {
+      const { chatId, telegramName } = user;
+
+      // Формуємо наше повідомлення для відповіді юзеру із окремих сегментів, для наглядності
+      let messageToUser = this.telegramUtilsService.setBotMessage({
+        lang: 'UA',
+        message: 'RETRIVED_USER_PROFILE',
+      });
+      messageToUser += `${this.telegramUtilsService.setBotMessage({
+        lang: 'UA',
+        message: 'YOUR_CHATID',
+      })}${chatId}\n`;
+      messageToUser += `${this.telegramUtilsService.setBotMessage({
+        lang: 'UA',
+        message: 'YOUR_NAME',
+      })}${telegramName}\n`;
+
+      // Бот надсилає повідомлення про успіх
+      try {
+        ctx.reply(messageToUser);
+      } catch (error) {
+        console.log('[Error send message] ::: ', error);
+      }
     }
   }
 
